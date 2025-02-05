5359d10a6a83b7426d096f8b11d2831714df74a4
thekungfukid <karthikkishor.4c@gmail.com>
2025-02-05 22:52:07 +0530

    TitleCards Updated

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,15 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './TitleCards.css'
-import cards_data from '../../assets/cards/Cards_data'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react';
+import './TitleCards.css';
+import { Link } from 'react-router-dom';
 
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef(null);
 
-const TitleCards = ({title,category}) => {
-  
-  const [apiData,setApiData] = useState([])
-  const cardsRef = useRef();
-
-  const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+  const url = `https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`;
   const options = {
     method: 'GET',
     headers: {
@@ -18,37 +15,42 @@ const TitleCards = ({title,category}) => {
     }
   };
 
-  
+  useEffect(() => {
+    fetch(url, options)
+      .then(res => res.json())
+      .then(json => setApiData(json.results))
+      .catch(err => console.error(err));
+  }, [category]); // Update data when category changes
 
-  const handleWheel=(event)=>{
-    event.preventDefault();
-    cardsRef.current.scrollLeft += event.deltaY;
-  }
+  useEffect(() => {
+    const cardsContainer = cardsRef.current;
+    if (!cardsContainer) return;
 
-  useEffect(()=>{
-    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
-    .then(res => res.json())
-    .then(json => setApiData(json.results))
-    .catch(err => console.error(err));
+    const handleWheel = (event) => {
+      event.preventDefault();
+      cardsContainer.scrollLeft += event.deltaY > 0 ? 100 : -100; // Adjust scrolling speed
+    };
 
-    cardsRef.current.addEventListener('wheel',handleWheel)
-  },[])
+    cardsContainer.addEventListener("wheel", handleWheel);
+    
+    return () => {
+      cardsContainer.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
 
   return (
-    <div className='title-cards'ref={cardsRef}>
-      <h2>{title?title:"Popular on Netflix"}</h2>
-      <div className='card-list'>
-        {
-        apiData.map((card,index)=>{
-            return <Link to={`/player/${card.id}`} className='card' key={index}>
-              <img src={`https://image.tmdb.org/t/p/w500/`+card.backdrop_path} alt="" />
-              <p>{card.original_title}</p>
-              </Link>
-          })
-        }
+    <div className="title-cards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => (
+          <Link to={`/player/${card.id}`} className="card" key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500/${card.backdrop_path}`} alt={card.original_title} />
+            <p>{card.original_title}</p>
+          </Link>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+export default TitleCards;
