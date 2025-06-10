6586af05bd4db2aa74efe9f5f5b3ce65914dea7c
karthikkishorkk <karthikkishor.4c@gmail.com>
2025-06-10 18:47:26 +0530

    ReelPlay Changes

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Login.css';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/ReelPlayLogo2.png';
 import {login,signup} from '../../firebase'
 import netflix_spinner from '../../assets/netflix_spinner.gif'
 
