e62761f68f38fbf076b042df319d4629f233c755
karthikkishorkk <karthikkishor.4c@gmail.com>
2025-07-18 15:55:33 +0530

    Footer Cleaned and Placed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,36 +1,31 @@
-import React from 'react'
-import './Footer.css'
-import youtube_icon from '../../assets/youtube_icon.png'
-import twitter_icon from '../../assets/twitter_icon.png'
-import instagram_icon from '../../assets/instagram_icon.png'
-import facebook_icon from '../../assets/facebook_icon.png'
+import React from 'react';
+import './Footer.css';
+import youtube_icon from '../../assets/youtube_icon.png';
+import twitter_icon from '../../assets/twitter_icon.png';
+import instagram_icon from '../../assets/instagram_icon.png';
+import facebook_icon from '../../assets/facebook_icon.png';
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="footer-icons">
-        <img src={youtube_icon} alt="" />
-        <img src={twitter_icon} alt="" />
-        <img src={instagram_icon} alt="" />
-        <img src={facebook_icon} alt="" />
+        <img src={youtube_icon} alt="YouTube" />
+        <img src={twitter_icon} alt="Twitter" />
+        <img src={instagram_icon} alt="Instagram" />
+        <img src={facebook_icon} alt="Facebook" />
       </div>
-      <ul>
-        <li>Audio Description</li>
-        <li>Help Centre</li>
-        <li>Gift Card</li>
-        <li>Media Centre</li>
-        <li>Investor Relations</li>
-        <li>Jobs</li>
-        <li>Terms of Use</li>
-        <li>Privacy</li>
-        <li>Legal Notices</li>
-        <li>Cookie Preferences</li>
-        <li>Corporate Information</li>
-        <li>Contact Us</li>
-      </ul>
-      <p className='copyright-text'>© KK's Project</p>
-    </div>
-  )
-}
 
-export default Footer
+      <ul className="footer-links">
+        <li>About ReelPlay</li>
+        <li>Contact</li>
+        <li>Privacy Policy</li>
+        <li>Terms & Conditions</li>
+        <li>Help & Support</li>
+      </ul>
+
+      <p className="copyright-text">© {new Date().getFullYear()} ReelPlay. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
