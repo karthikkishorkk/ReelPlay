9398dd369c1711c5b4242bfc51000e59720f8229
karthikkishorkk <karthikkishor.4c@gmail.com>
2025-07-18 15:49:39 +0530

    Navbar Updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/ReelPlayLogo2.png';
-import search_icon from '../../assets/search_icon.svg';
 import bell_icon from '../../assets/bell_icon.svg';
 import profile_img from '../../assets/profile_img.png';
 import caret_icon from '../../assets/caret_icon.svg';
@@ -9,6 +8,7 @@ import { logout } from '../../firebase';
 
 const Navbar = () => {
   const navRef = useRef();
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,36 +20,30 @@ const Navbar = () => {
     };
 
     window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll); // ✅ Cleanup event listener
-    };
-  }, []); // ✅ Runs only once
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
 
   return (
-    <div className='navbar' ref={navRef}> {/* ✅ Assigned ref here */}
-      <div className='navbar-left'>
-        <img src={logo} alt="" />
-        <ul>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
-          <li>Browse by Languages</li>
-        </ul>
+    <div className="navbar" ref={navRef}>
+      <div className="navbar-left">
+        <img src={logo} alt="ReelPlay Logo" className="logo" />
+        {/* Navigation options removed */}
       </div>
 
-      <div className='navbar-right'>
-        <img src={search_icon} alt="" className='icons' />
-        <p>Children</p>
-        <img src={bell_icon} alt="" className='icons' />
-        <div className='navbar-profile'>
-          <img src={profile_img} alt="" className='profile' />
-          <img src={caret_icon} alt="" />
-          <div className='dropdown'>
-            <p onClick={()=>{logout()}}>Sign out of Netflix</p>
-          </div>
+      <div className="navbar-right">
+        <img src={bell_icon} alt="Notifications" className="icons" />
+        <div className="navbar-profile" onClick={toggleDropdown}>
+          <img src={profile_img} alt="Profile" className="profile" />
+          <img src={caret_icon} alt="Dropdown" className="caret" />
+          {dropdownVisible && (
+            <div className="dropdown">
+              <p onClick={() => logout()}>Sign out of ReelPlay</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
